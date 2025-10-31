da9f9acb8861e205391eb7f4e09037f88649d485
MaximeCaute <48546600+MaximeCaute@users.noreply.github.com>
2025-10-31 15:05:27 +0100

    Added distant saving

--- common/src/utils/saving-utils.js
+++ common/src/utils/saving-utils.js
@@ -3,4 +3,12 @@ class SavingUtils {
     console.log("Saving data locally !")
     jsPsych.data.get().localSave('csv',subjectID+".csv");
   }
+
+  // Requires a functional write_data.php
+  static saveDataPHP(name, data){
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'write_data.php');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({fileprefix: name, filedata: data}));
+  }
 }
